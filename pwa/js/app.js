7a79b314712b72b159b2581e2c27a53ae21d3ddf
Chris Bowers <pcbowers@gmail.com>
2018-12-13 19:29:09 +0100

    updated service-worker.js name in app.js

--- pwa/js/app.js
+++ pwa/js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator){
     window.addEventListener('load', function(){
-        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
             console.log('[EReporter - ServiceWorker] registration successful with scope: ', registration.scope);
         }, function(error){
             console.log('[EReporter - ServiceWorker] registration failed: ', error);
